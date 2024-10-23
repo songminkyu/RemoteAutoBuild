3d4d344310aeab1ac7e067a85ddce9fde6553167
song minkyu <mksong@finaldata.com>
2024-10-23 09:19:29 +0900

    Log 추가

--- AutoBuild/Controllers/ProductInfoController.cs
+++ AutoBuild/Controllers/ProductInfoController.cs
@@ -18,32 +18,43 @@ namespace AutoBuild.Controllers
         [Route("run-file")]
         public IActionResult RunFile([FromBody] ProductInfo productInfo)
         {
-            ProcessStartInfo startInfo = new ProcessStartInfo();
-            startInfo.FileName = "python";            
-            startInfo.UseShellExecute = false;
-            startInfo.CreateNoWindow = false;
-            if (productInfo.ProductName == Enums.ProductCategory.FFR)
+            try
             {
-                startInfo.Arguments = @"D:\Source\AutoBuild\Scripts\AutoBuild_R_FF.py";              
+                ProcessStartInfo startInfo = new ProcessStartInfo();
+                startInfo.FileName = "python";
+                startInfo.UseShellExecute = false;
+                startInfo.CreateNoWindow = false;
+                if (productInfo.ProductName == Enums.ProductCategory.FFR)
+                {
+                    startInfo.Arguments = @"D:\Source\AutoBuild\Scripts\AutoBuild_R_FF.py";
+                }
+                else if (productInfo.ProductName == Enums.ProductCategory.FFR_Portable)
+                {
+                    startInfo.Arguments = @"D:\Source\AutoBuild\Scripts\AutoBuild_R_FF_Portable.py 0 1";
+                }
+                else if (productInfo.ProductName == Enums.ProductCategory.FMFR)
+                {
+                    startInfo.Arguments = @"D:\Source\AutoBuild\Scripts\AutoBuild_R_FMF.py";
+                }
+                else
+                {
+                    startInfo.Arguments = @"D:\Source\AutoBuild\Scripts\AutoBuild_R_FML.py";
+                }
+                using (Process process = Process.Start(startInfo))
+                {
+                    // 프로세스가 종료될 때까지 기다림
+                    process.WaitForExit();                    
+                }
+                string res = string.Format("{0} 빌드가 완료 되었습니다", productInfo.ProductName);
+                _logger.LogInformation(res);
+                return Ok(res);
             }
-            else if (productInfo.ProductName == Enums.ProductCategory.FFR_Portable)
+            catch (Exception ex) 
             {
-                startInfo.Arguments = @"D:\Source\AutoBuild\Scripts\AutoBuild_R_FF_Portable.py 0 1";
-            }
-            else if(productInfo.ProductName == Enums.ProductCategory.FMFR)
-            {
-                startInfo.Arguments = @"D:\Source\AutoBuild\Scripts\AutoBuild_R_FMF.py";
-            }
-            else
-            {
-                startInfo.Arguments = @"D:\Source\AutoBuild\Scripts\AutoBuild_R_FML.py";
-            }
-            using (Process process = Process.Start(startInfo))
-            {
-                // 프로세스가 종료될 때까지 기다림
-                process.WaitForExit();
+                string exMessage = string.Format("Build Error : {0}", ex.Message);
+                _logger.LogError(exMessage);
+                return NotFound(exMessage);
             }            
-            return Ok("File executed successfully.");                       
         }
     }
 }
